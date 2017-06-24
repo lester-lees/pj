af375665a64560f387aff0d38889278df0180729
jimmy_lees <346994202@qq.com>
2017-06-24 12:27:25 +0800

    add odoo module importing statement

--- t.py
+++ t.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 import os,re
+# -*- coding: utf-8 -*-
+from openerp.osv import fields, osv
+from datetime import datetime,timedelta
+import xmlrpclib
+from openerp.tools.translate import _
+
 
 """description for this module"""
 
